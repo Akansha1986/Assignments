40eb3d5e82b0be0c7c5435b55e76abb80f4694d3
Akansha1986 <agrawalakansha.21@gmail.com>
2025-07-07 15:43:22 +0530

    Assignments

--- src/assignments/Assignment1.java
+++ src/assignments/Assignment1.java
@@ -3,8 +3,49 @@ package assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Assignment 1 – Data Types
+		float tempratureOfCity = (float)25.5;
+		boolean orderPlace = true;
+		long personPhoneNumber = 1234567890;
+		double amountOfmoneyInAccount = 1000.50;
+		String emailId = "john.due@example.com";
+		double latitude = -122.4194;
+		double longitutde = 37.7749;
+		boolean martialStatus = true;
+		String personOccupation ="Software Tester";
+		String favouriteColour = "Yellow";
+		int currentYear = 2005;
+		int numberOffollower = 10000;
+		float ratingOfMovie = (float) 7.5;
+		char bloodGroup = 'A';
+		String titleOfBook =" To Kill a Mockingbird";
+		int numberOfEmplyeeIncompany = 500;
+		String timeOfEvent = "2:30 PM";
+		String nameOfCountry = "United State";
+		String personEye = "Brown";
+		String placeOfBirth = "New York City";
+		float distanceBetweenTwoCity = (float) 200.5;
+		System.out.println("1. Temperature of a city in degrees Celsius:"+tempratureOfCity);
+		System.out.println("2. Whether a customer has placed an order:" +orderPlace);
+		System.out.println("3. Person's phone number:" +personPhoneNumber);
+		System.out.println("4. Amount of money in a customer's bank account:"+amountOfmoneyInAccount);
+		System.out.println("5.1 Coordinates of a location longitutde :"+longitutde);
+		System.out.println("5.2 Coordinates of a location latitude:"+latitude);
+		System.out.println("6. Person's email address:"+ emailId);
+		System.out.println("7. Person's marital status:"+ martialStatus);
+		System.out.println("8. Person's occupation:"+personOccupation);
+		System.out.println("9. Person's favourite colour:"+favouriteColour);
+		System.out.println("10.Current year: "+currentYear);
+		System.out.println("11.Number of followers on a social media platform:"+numberOffollower);
+		System.out.println("12.Rating of a movie:"+ratingOfMovie);
+		System.out.println("13.Person's blood type:"+bloodGroup);
+		System.out.println("14.Title of a book:" +titleOfBook);
+		System.out.println("15.Number of employees in a company:"+numberOfEmplyeeIncompany);
+		System.out.println("16.Time of an event"+timeOfEvent);
+		System.out.println("17.Name of a country:"+nameOfCountry);
+		System.out.println("18.Person's eye color:"+personEye);
+		System.out.println("19.Person's birthplace: "+placeOfBirth);
+		System.out.println("20.Distance between two cities:" +  distanceBetweenTwoCity);
 	}
 
 }
